ac892f2cb8506ec9b5cb0dabb405ba332a2894e0
Nikdxt <nikhildixit10071999@gmail.com>
2024-08-14 15:57:33 +0530

    school project

---
+++ src/test/java/com/example/schoolmanagement/SchoolManagementAppTest.java
@@ -0,0 +1,11 @@
+package com.example.schoolmanagement;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SchoolManagementAppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
