f6cc4cf6240108b5c0b537dc0b68934f80e9c57a
Nikdxt <nikhildixit10071999@gmail.com>
2024-08-15 09:33:35 +0530

    deleting project

--- src/main/java/com/example/schoolmanagement/SchoolManagementApp.java
+++
@@ -1,7 +0,0 @@
-package com.example.schoolmanagement;
-
-public class SchoolManagementApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the School Management System");
-    }
-}
